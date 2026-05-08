2b2eeb03b6337f49f46fccc6c5f7a1dd890f6d61
Anusha-Darapureddi <anushadarapureddi3@gmail.com>
2026-05-08 22:20:22 +0530

    Add files via upload

---
+++ 15day.py
@@ -0,0 +1,56 @@
+# n1=0
+# n2=1
+# num=int(input("enter:"))
+# print(n1,n2,end=" ")
+# for i in range(1,num+1):
+#     sum=n1+n2
+#     n1=n2
+#     n2=sum
+#     print(sum,end=" ")
+    
+    
+# num=int(input("enter a number:"))
+# total=0
+# length=len(str(num))
+# for i in str(num):
+#     total+=int(i)**length
+# if(num==total):
+#     print(f"{num} is a armstrong")
+# else:
+#     print(f"{num} is not a armstrong number")
+    
+
+# num=int(input("enter:"))
+# if(num%3==0 and num%5==0):
+#     print(f"{num} is divisible by both 3 and 5")
+# else:
+#     print(f"{num} is not divisible by both 3 and 5 ")
+
+
+# num=100
+# for i in range(1,num+1):
+#     if(i%3==0 and i%5==0):
+#         print(f"{i} is divisible by both 3 and 5")
+
+
+# li=[3,5,2,6,8]
+
+# def sum_even(li):
+#     sum=0
+#     for i in li:
+#         if(i%2==0):
+#             sum+=i
+#     print(sum)
+# sum_even(li)
+
+
+#lambda function
+#-------------A lambda function is a small anonymous function 
+#-------------this lambda function can take n number of arguments but can only have one expression
+
+#syntax -->lambda keyword (arguments):expression
+
+# a=lambda a,b:a+b
+# print(a(2,4))
+
+
